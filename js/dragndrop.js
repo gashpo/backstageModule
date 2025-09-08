fc7e6b767b3c36d199a3af21e6d6401dc4ce8d58
gashpo <gashpofigma@gmail.com>
2025-09-08 15:44:59 +0800

    update datatable fixed-column

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -1,17 +1,3 @@
-var example1 = document.getElementById("example1"),
-  example2Left = document.getElementById("example2-left"),
-  example2Right = document.getElementById("example2-right"),
-  example3Left = document.getElementById("example3-left"),
-  example3Right = document.getElementById("example3-right"),
-  example4Left = document.getElementById("example4-left"),
-  example4Right = document.getElementById("example4-right"),
-  example5 = document.getElementById("example5"),
-  example6 = document.getElementById("example6"),
-  example7 = document.getElementById("example7"),
-  gridDemo = document.getElementById("gridDemo"),
-  multiDragDemo = document.getElementById("multiDragDemo"),
-  swapDemo = document.getElementById("swapDemo");
-
 var nestedSortables = [].slice.call(
   document.querySelectorAll(".nested-sortable")
 );
